59cb0e56e37ce2113f791bec7590863b12919782
Gabriel Dulac-Arnold <gabe@squirrelsoup.net>
2020-03-24 15:09:56 +0100

    Now need to specify location of .env file by updating config.py

--- icubam/config.py
+++ icubam/config.py
@@ -2,8 +2,8 @@
 from dotenv import load_dotenv
 import os
 
-
-load_dotenv(verbose=True)
+ENV_PATH =
+load_dotenv(verbose=True, dotenv_path=ENV_PATH)
 envs = ("SHEET_ID,TOKEN_LOC,SMS_KEY,SMS_ORIG,SECRET_COOKIE,JWT_SECRET,"
         "SQLITE_DB,GOOGLE_API_KEY")
 for env in envs.split(','):
