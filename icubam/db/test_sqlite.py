63c019a48568cce69bb4f8182cd6f13eb29b548b
Romain PRIMET <romain.primet@inria.fr>
2020-03-25 12:58:40 +0100

    fix last sqlite test (previously commented out)

--- icubam/db/test_sqlite.py
+++ icubam/db/test_sqlite.py
@@ -46,33 +46,33 @@ class SQLiteDBTest(absltest.TestCase):
           sqldb.add_user("ICU1", "Bob", "+33698158092", "Chercheur")
         users = sqldb.get_users()
 
-  # def test_bedcount_update(self):
-  #   tmp_folder = self.create_tempdir()
-  #   sqldb = SQLiteDB(os.path.join(tmp_folder, "test.db"))
+  def test_bedcount_update(self):
+    with tempfile.TemporaryDirectory() as tmp_folder:
+        sqldb = sqlite.SQLiteDB(os.path.join(tmp_folder, "test.db"))
 
-  #   # Make sure you can't insert without a valid icu_id
-  #   with self.assertRaises(ValueError):
-  #     sqldb.update_bedcount(1, "test", 10, 9, 8, 7, 6, 5, 4)
-  #   sqldb.upsert_icu("ICU1", "dep1", "city1", 3.44, 42.3, "0102")
-  #   sqldb.upsert_icu("ICU2", "dep1", "city1", 3.44, 42.3, "0102")
+        # Make sure you can't insert without a valid icu_id
+        with self.assertRaises(ValueError):
+          sqldb.update_bedcount(1, "test", 10, 9, 8, 7, 6, 5, 4)
+        sqldb.upsert_icu("ICU1", "dep1", "city1", 3.44, 42.3, "0102")
+        sqldb.upsert_icu("ICU2", "dep1", "city1", 3.44, 42.3, "0102")
 
-  #   # Generate some bed updates:
-  #   for i in [1, 2]:
-  #     for j in range(10):
-  #       time.sleep(0.5)
-  #       sqldb.update_bedcount(i, "test", 10, 9, 8, 7, 6, 5, 4)
-  #   bedcount = sqldb.get_bedcount()
-  #   self.assertLen(bedcount, 2)
+        # Generate some bed updates:
+        for i in [1, 2]:
+          for j in range(10):
+            time.sleep(0.01)
+            sqldb.update_bedcount(i, "test", 10, 9, 8, 7, 6, 5, 4)
+        bedcount = sqldb.get_bedcount()
+        self.assertLen(bedcount, 2)
 
-  #   # Make sure the returned updates are the most recent
-  #   for i in [1, 2]:
-  #     res = sqldb.execute(
-  #       f"SELECT MAX(update_ts) as max_ts FROM bed_updates WHERE icu_id = {i}"
-  #     )
-  #     max_ts = res.iloc[0]["max_ts"]
-  #     self.assertEqual(
-  #       bedcount[bedcount["icu_id"] == i].iloc[0]["update_ts"], max_ts
-  #     )
+        # Make sure the returned updates are the most recent
+        for i in [1, 2]:
+          res = sqldb.pd_execute(
+            f"SELECT MAX(update_ts) as max_ts FROM bed_updates WHERE icu_id = {i}"
+          )
+          max_ts = res.iloc[0]["max_ts"]
+          self.assertEqual(
+            bedcount[bedcount["icu_id"] == i].iloc[0]["update_ts"], max_ts
+          )
 
 if __name__ == "__main__":
   absltest.main()
