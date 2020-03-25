9bbd2ebb02009d795391dd399d8d06274953a2c8
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2020-03-25 14:04:07 +0100

    update form and add modify option in show view

--- icubam/www/handlers/update.py
+++ icubam/www/handlers/update.py
@@ -7,55 +7,42 @@ from icubam.www import token
 
 
 class UpdateHandler(base.BaseHandler):
+    ROUTE = '/update'
+    QUERY_ARG = 'id'
 
-  ROUTE = '/update'
-  QUERY_ARG = 'id'
+    def initialize(self, db, queue):
+        self.db = db
+        self.queue = queue
 
-  def initialize(self, db, queue):
-    self.db = db
-    self.queue = queue
+    def get_icu_data_by_id(self, icu_id, def_val=0):
+        df = None
+        try:
+            df = self.db.get_bedcount_by_icu_id(icu_id)
+        except Exception as e:
+            logging.error(e)
+        result = df.to_dict(orient="records")[0] if df is not None and not df.empty else None
+        return result
 
-  def get_icu_data(self, icu_id, def_val=0):
-    df = self.db.get_bedcount()
-    try:
-      data = None
-      for index, row in df[df.icu_id == icu_id].iterrows():
-        data = row.to_dict()
-        break
-    except Exception as e:
-      logging.error(e)
-      data = {x: def_val for x in df.columns.to_list() if x.startswith('n_')}
+    @tornado.web.authenticated
+    async def get(self):
+        """Serves the page with a form to be filled by the user."""
+        icu_id = self.get_query_argument("icu_id", None, True)
+        if icu_id is None or self.get_icu_data_by_id(icu_id) == None:
+            return self.redirect('/error')
 
-    if data is None:
-      data = {x: def_val for x in df.columns.to_list() if x.startswith('n_')}
+        data = self.get_icu_data_by_id(icu_id)
 
-    for k in data:
-      if data[k] is None:
-        data[k] = def_val
+        # self.set_secure_cookie(self.COOKIE, user_token)
+        self.render('update_form.html', **data)
 
-    return data
+    @tornado.web.authenticated
+    async def post(self):
+        def parse(param):
+            parts = param.split('=')
+            value = int(parts[1]) if parts[1].isnumeric() else 0
+            return parts[0], value
 
-  async def get(self):
-    """Serves the page with a form to be filled by the user."""
-    user_token = self.get_query_argument(self.QUERY_ARG)
-    input_data = token.decode(user_token)
-
-    if input_data is None:
-      return self.redirect('/error')
-
-    data = self.get_icu_data(input_data['icu_id'])
-    data.update(input_data)
-
-    self.set_secure_cookie(self.COOKIE, user_token)
-    self.render('update_form.html', **data)
-
-  async def post(self):
-    def parse(param):
-      parts = param.split('=')
-      value = int(parts[1]) if parts[1].isnumeric() else 0
-      return parts[0], value
-
-    data = dict([parse(p) for p in self.request.body.decode().split('&')])
-    data.update(token.decode(self.get_secure_cookie(self.COOKIE)))
-    await self.queue.put(data)
-    self.redirect(home.HomeHandler.ROUTE)
+        data = dict([parse(p) for p in self.request.body.decode().split('&')])
+        data.update(token.decode(self.get_secure_cookie(self.COOKIE)))
+        await self.queue.put(data)
+        self.redirect(home.HomeHandler.ROUTE)
--- icubam/www/static/show.js
+++ icubam/www/static/show.js
@@ -8,15 +8,15 @@ $(document).ready(function() {
             return d
         },
     },
-    /*columnDefs: [ {
+    columnDefs: [ {
         "targets": 0,
         "data": "icu_id",
         "render": function ( data, type, row, meta ) {
-              return '<a href="update?id='+row.icu_id+'"><span class="glyphicon glyphicon-edit aria-hidden="true"">modify </span></a>'; }
-    } ],*/
+              return '<a href="update?icu_id='+row.icu_id+'"><span class="glyphicon glyphicon-edit aria-hidden="true"">modify </span></a>'; }
+    } ],
 
     columns: [
-    //{data: 'icu_id'},
+    {data: 'icu_id'},
     {data: 'icu_name'},
     {data: 'n_covid_occ'},
     {data: 'n_covid_free'},
