02ce0b5c1f8ce1601c2a2796f5d1318fe17bd6b7
Gabriel Dulac-Arnold <gabriel@technonautes.fr>
2020-03-25 15:25:27 +0000

    Updated flags.

--- scripts/populate_db.py
+++ scripts/populate_db.py
@@ -6,12 +6,13 @@ from icubam.db import gsheets
 from icubam.db import synchronizer
 
 flags.DEFINE_string('config', 'resources/config.toml', 'Config file.')
+flags.DEFINE_string('dotenv_path', 'resources/.env', 'Config file.')
 flags.DEFINE_enum('mode', 'dev', ['prod', 'dev'], 'Run mode.')
 FLAGS = flags.FLAGS
 
 
 def main(unused_argv):
-  cfg = config.Config(FLAGS.config, mode=FLAGS.mode)
+  cfg = config.Config(FLAGS.config, mode=FLAGS.mode, env_path=FLAGS.dotenv_path)
   shdb = gsheets.SheetsDB(cfg.TOKEN_LOC, cfg.SHEET_ID)
   sqldb = sqlite.SQLiteDB(cfg.db.sqlite_path)
   sync = synchronizer.Synchronizer(shdb, sqldb)
