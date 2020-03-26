b31db673a267b618d837ce28bd7c72adf95df3c7
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2020-03-26 11:32:01 +0100

    indentation 2 spaces

--- icubam/www/handlers/show.py
+++ icubam/www/handlers/show.py
@@ -4,28 +4,28 @@ from icubam.www.handlers import base
 
 
 class DataJson(base.BaseHandler):
-    ROUTE = '/beds'
+  ROUTE = '/beds'
 
-    def initialize(self, db):
-        self.db = db
+  def initialize(self, db):
+    self.db = db
 
-    def get_icu_data(self):
-        df = self.db.get_bedcount()
-        return df
+  def get_icu_data(self):
+    df = self.db.get_bedcount()
+    return df
 
-    @tornado.web.authenticated
-    def get(self):
-        data = self.get_icu_data().to_dict(orient="records")
-        self.write({"data": data})
+  @tornado.web.authenticated
+  def get(self):
+    data = self.get_icu_data().to_dict(orient="records")
+    self.write({"data": data})
 
 
 class ShowHandler(base.BaseHandler):
-    ROUTE = '/show'
+  ROUTE = '/show'
 
-    def initialize(self, db):
-        self.db = db
+  def initialize(self, db):
+    self.db = db
 
-    @tornado.web.authenticated
-    def get(self):
-        """Serves the page beds"""
-        self.render("show.html")
+  @tornado.web.authenticated
+  def get(self):
+    """Serves the page beds"""
+    self.render("show.html")
--- icubam/www/handlers/update.py
+++ icubam/www/handlers/update.py
@@ -8,57 +8,57 @@ from icubam.www import token
 
 
 def time_ago(ts) -> str:
-    if ts is None:
-        return 'jamais'
+  if ts is None:
+    return 'jamais'
 
-    delta = int(time.time() - int(ts))
-    units = [(86400, 'jour'), (3600, 'heure'), (60, 'minute'), (1, 'seconde')]
-    for unit, name in sorted(units, reverse=True):
-        curr = delta // unit
-        if curr > 0:
-            plural = '' if curr == 1 else 's'  # hack
-            return 'il y a {} {}{}'.format(curr, name, plural)
-    return 'à l\'instant'
+  delta = int(time.time() - int(ts))
+  units = [(86400, 'jour'), (3600, 'heure'), (60, 'minute'), (1, 'seconde')]
+  for unit, name in sorted(units, reverse=True):
+    curr = delta // unit
+    if curr > 0:
+      plural = '' if curr == 1 else 's'  # hack
+      return 'il y a {} {}{}'.format(curr, name, plural)
+  return 'à l\'instant'
 
 
 class UpdateHandler(base.BaseHandler):
-    ROUTE = '/update'
-    QUERY_ARG = 'id'
+  ROUTE = '/update'
+  QUERY_ARG = 'id'
 
-    def initialize(self, db, queue, token_encoder):
-        self.db = db
-        self.queue = queue
-        self.token_encoder = token_encoder
+  def initialize(self, db, queue, token_encoder):
+    self.db = db
+    self.queue = queue
+    self.token_encoder = token_encoder
 
-    def get_icu_data_by_id(self, icu_id):
-        df = None
-        try:
-            df = self.db.get_bedcount_by_icu_id(icu_id)
-        except Exception as e:
-            logging.error(e)
-        result = df.to_dict(orient="records")[0] if df is not None and not df.empty else None
-        return result
+  def get_icu_data_by_id(self, icu_id):
+    df = None
+    try:
+      df = self.db.get_bedcount_by_icu_id(icu_id)
+    except Exception as e:
+      logging.error(e)
+    result = df.to_dict(orient="records")[0] if df is not None and not df.empty else None
+    return result
 
-    @tornado.web.authenticated
-    async def get(self):
-        """Serves the page with a form to be filled by the user."""
-        icu_id = self.get_query_argument("icu_id", None, True)
-        if icu_id is None or self.get_icu_data_by_id(icu_id) == None:
-            return self.redirect('/error')
+  @tornado.web.authenticated
+  async def get(self):
+    """Serves the page with a form to be filled by the user."""
+    icu_id = self.get_query_argument("icu_id", None, True)
+    if icu_id is None or self.get_icu_data_by_id(icu_id) == None:
+      return self.redirect('/error')
 
-        data = self.get_icu_data_by_id(icu_id)
+    data = self.get_icu_data_by_id(icu_id)
 
-        # self.set_secure_cookie(self.COOKIE, user_token)
-        self.render('update_form.html', **data)
+    # self.set_secure_cookie(self.COOKIE, user_token)
+    self.render('update_form.html', **data)
 
-    @tornado.web.authenticated
-    async def post(self):
-        def parse(param):
-            parts = param.split('=')
-            value = int(parts[1]) if parts[1].isnumeric() else 0
-            return parts[0], value
+  @tornado.web.authenticated
+  async def post(self):
+    def parse(param):
+      parts = param.split('=')
+      value = int(parts[1]) if parts[1].isnumeric() else 0
+      return parts[0], value
 
-        data = dict([parse(p) for p in self.request.body.decode().split('&')])
-        data.update(self.token_encoder.decode(self.get_secure_cookie(self.COOKIE)))
-        await self.queue.put(data)
-        self.redirect(home.HomeHandler.ROUTE)
+    data = dict([parse(p) for p in self.request.body.decode().split('&')])
+    data.update(self.token_encoder.decode(self.get_secure_cookie(self.COOKIE)))
+    await self.queue.put(data)
+    self.redirect(home.HomeHandler.ROUTE)
