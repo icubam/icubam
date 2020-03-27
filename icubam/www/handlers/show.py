387d75aeb988de61ba65c5289b149a86903d6e58
Deathn0t <romainegele@gmail.com>
2020-03-27 18:05:15 +0100

    update tabs

--- icubam/www/handlers/show.py
+++ icubam/www/handlers/show.py
@@ -7,26 +7,26 @@ from icubam.www.handlers.update import time_ago
 
 
 class DataJson(base.BaseHandler):
-	ROUTE = '/beds'
+  ROUTE = '/beds'
 
-	def initialize(self, db):
-		self.db = db
+  def initialize(self, db):
+	  self.db = db
 
-	def get_icu_data(self):
-		df = self.db.get_bedcount()
-		df['since_update'] = df.update_ts.apply(time_ago)
-		n_covid_tot = df['n_covid_free'] + df['n_covid_occ']
-		df.insert(1, 'n_covid_tot', n_covid_tot)
-		return df
+  def get_icu_data(self):
+	  df = self.db.get_bedcount()
+	  df['since_update'] = df.update_ts.apply(time_ago)
+	  n_covid_tot = df['n_covid_free'] + df['n_covid_occ']
+	  df.insert(1, 'n_covid_tot', n_covid_tot)
+	  return df
 
-	@tornado.web.authenticated
-	def get(self):
-		data = self.get_icu_data().to_dict(orient="records")
-		# icu_id = self.db.get_icu_id_from_name("Troyes")
-		# tmp = self.db.get_update_ts(icu_ids=[icu_id])
-		# print(tmp.describe())
-		# print(tmp.head())
-		self.write({"data": data})
+  @tornado.web.authenticated
+  def get(self):
+    data = self.get_icu_data().to_dict(orient="records")
+	  # icu_id = self.db.get_icu_id_from_name("Troyes")
+	  # tmp = self.db.get_update_ts(icu_ids=[icu_id])
+	  # print(tmp.describe())
+	  # print(tmp.head())
+    self.write({"data": data})
 
 
 class ShowHandler(base.BaseHandler):
@@ -35,7 +35,7 @@ class ShowHandler(base.BaseHandler):
   def initialize(self, db):
     self.db = db
 
-    # @tornado.web.authenticated
+  @tornado.web.authenticated
   async def get(self):
     """Serves the page with a form to be filled by the user."""
     self.render("show.html")
