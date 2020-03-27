5743f99946128a0977eaefcabe77f689a053c4af
Olivier Teboul <oliviert@google.com>
2020-03-27 20:06:18 +0100

    fixing script

--- scripts/ib_send_sms.py
+++ scripts/ib_send_sms.py
@@ -4,15 +4,15 @@ from icubam.db import gsheets
 from icubam.messaging import mb_sender
 
 flags.DEFINE_string('config', 'resources/config.toml', 'Config file.')
+flags.DEFINE_string('dotenv_path', None, 'Optionally specifies the .env path.')
 flags.DEFINE_enum('mode', 'dev', ['prod', 'dev'], 'Run mode.')
 FLAGS = flags.FLAGS
 
-def main(unused_argv):
-  cfg = config.Config(FLAGS.config, mode=FLAGS.mode)
+
+def main(argv):
+  cfg = config.Config(FLAGS.config, mode=FLAGS.mode, env_path=FLAGS.dotenv_path)
   sdb = gsheets.SheetsDB(cfg.TOKEN_LOC, cfg.SHEET_ID)
-  sender = mb_sender.MBSender(
-    api_key=config.SMS_KEY, originator=cfg.sms.origin
-  )
+  sender = mb_sender.MBSender(cfg)
 
   users = sdb.get_users()
   print(users)
@@ -20,7 +20,7 @@ def main(unused_argv):
     print(row)
     tel = row[1]["tel"]
     print(tel)
-    sender.send_message(tel, "Test")
+    sender.send(tel, "Test")
 
 
 if __name__ == "__main__":
