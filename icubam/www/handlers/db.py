3900858844b4b673a6cdef2d48e12d4d684653f2
Gabriel Dulac-Arnold <gabriel@technonautes.fr>
2020-03-27 11:47:39 +0100

    Added CSV export.

--- icubam/www/handlers/db.py
+++ icubam/www/handlers/db.py
@@ -13,8 +13,13 @@ class DBHandler(base.BaseHandler):
     self.get_fns = {k: getattr(self.db, f'get_{k}', None) for k in keys}
 
   def get(self, collection):
+    print(collection)
     get_fn = self.get_fns.get(collection, None)
+    do_csv = self.get_query_argument('csv', default=None)
     if get_fn is not None:
-      self.write(get_fn().to_html())
+      if do_csv:
+        self.write(get_fn().to_csv())
+      else:
+        self.write(get_fn().to_html())
     else:
       self.redirect(home.HomeHandler.ROUTE)
