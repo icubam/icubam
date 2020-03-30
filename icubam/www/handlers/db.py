094296063402968c04cd248e110de9e7aad21309
Valentin Iovene <valentin@too.gy>
2020-03-31 00:27:27 +0200

    hdf + csv working

--- icubam/www/handlers/db.py
+++ icubam/www/handlers/db.py
@@ -1,6 +1,7 @@
 import tempfile
 import logging
 import datetime
+import io
 import os
 
 import pandas as pd
@@ -10,6 +11,21 @@ from icubam.www.handlers import base
 from icubam.www.handlers import home
 
 access_log = logging.getLogger('tornado.access')
+
+def _get_headers(collection, asked_file_type):
+  if asked_file_type not in {'csv', 'hdf'}:
+    return dict()
+  extension = 'csv' if asked_file_type == 'csv' else 'h5'
+  datestr = datetime.datetime.now().strftime('%Y-%m-%d_%Hh%M')
+  filename = f'{collection}_{datestr}.{extension}'
+  content_type = (
+    'text/csv' if asked_file_type == 'csv' else 'application/octetstream'
+  )
+  headers = {
+    'Content-Type': content_type,
+    'Content-Disposition': f'attachment; filename={filename}'
+  }
+  return headers
 
 class DBHandler(base.BaseHandler):
 
@@ -23,7 +39,6 @@ class DBHandler(base.BaseHandler):
 
   def prepare(self):
     file_name = datetime.datetime.now().strftime('%Y-%m-%d_%Hh%M')
-    self.set_header('Content-Type', 'application/octet-stream')
     self.set_header(
       "Content-Disposition", f"attachment; filename=bedcount_{file_name}.h5"
     )
@@ -31,30 +46,35 @@ class DBHandler(base.BaseHandler):
   @tornado.web.authenticated
   def get(self, collection):
     get_fn = self.get_fns.get(collection, None)
-    hdf = self.get_query_argument('hdf', default=None)
-    max_ts = self.get_query_argument('max_ts', default=None)
-    access_log.debug(f'seen request: {get_fn.__name__}')
-    access_log.debug(f'\tcollection = {collection}')
-    access_log.debug(f'\thdf = {hdf}')
-    access_log.debug(f'\tmax_ts = {max_ts}')
-    if get_fn is not None:
-      if hdf:
-        if collection == 'bedcount':
-          with tempfile.NamedTemporaryFile() as f:
-            tmp_path = f.name
-          data = get_fn(max_ts=max_ts)
-          data.to_hdf(
-            tmp_path,
-            key='data',
-            complib='blosc:lz4',
-            complevel=9,
-          )
-          with open(tmp_path, 'rb') as f:
-            self.write(f.read())
-          os.remove(tmp_path)
-        else:
-          self.write(get_fn().to_csv())
-      else:
-        self.write(get_fn().to_html())
-    else:
+    if get_fn is None:
       self.redirect(home.HomeHandler.ROUTE)
+    hdf = self.get_query_argument('hdf', default=None)
+    csv = self.get_query_argument('csv', default=None)
+    max_ts = self.get_query_argument('max_ts', default=None)
+    data = get_fn(max_ts=max_ts)
+    asked_file_type = None
+    if hdf: asked_file_type = 'hdf'
+    if csv: asked_file_type = 'csv'
+    access_log.debug(f'request on collection {collection}')
+    access_log.debug(f'asked file type: {asked_file_type}')
+    for k, v in _get_headers(collection, asked_file_type).items():
+      self.set_header(k, v)
+      access_log.debug(f'setting header {k} = {v}')
+    if asked_file_type == 'csv':
+      stream = io.StringIO()
+      data.to_csv(stream)
+      self.write(stream.getvalue())
+    elif asked_file_type == 'hdf':
+      with tempfile.NamedTemporaryFile() as f:
+        tmp_path = f.name
+      data.to_hdf(
+        tmp_path,
+        key='data',
+        complib='blosc:lz4',
+        complevel=9,
+      )
+      with open(tmp_path, 'rb') as f:
+        self.write(f.read())
+      os.remove(tmp_path)
+    else:
+      self.write(data.to_html())
