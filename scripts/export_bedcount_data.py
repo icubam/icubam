0aa8b369e4f46b49d91c308d92643ac453836889
Valentin Iovene <valentin@too.gy>
2020-03-30 21:37:20 +0200

    add script to export bedcount data

---
+++ scripts/export_bedcount_data.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from absl import app
+from absl import flags
+
+from icubam.www.handlers.db import DBHandler
+from icubam.db import sqlite
+
+flags.DEFINE_string("db_path", "test.db", "Path to SQLite3 database.")
+flags.DEFINE_string('out_path', '/tmp/bedcount.h5', 'Output file.')
+FLAGS = flags.FLAGS
+
+def main(argv):
+  if not FLAGS.out_path.endswith(".h5"):
+    raise ValueError(
+      f"Expected output file to end with `.h5'. Got {FLAGS.out_path} instead"
+    )
+  sqldb = sqlite.SQLiteDB(FLAGS.db_path)
+  data = sqldb.get_bedcount()
+  __import__('pdb').set_trace()
+  data.to_hdf(FLAGS.out_path, 'data')
+
+if __name__ == "__main__":
+  app.run(main)
