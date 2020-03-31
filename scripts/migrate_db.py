05b688560056e1dbe703dac1756b1c8e2a2489fc
Olivier Teboul <oliviert@google.com>
2020-03-31 18:02:55 +0200

    migration complete

--- scripts/migrate_db.py
+++ scripts/migrate_db.py
@@ -1,9 +1,11 @@
 from absl import app
 from absl import flags
 from icubam import config
+from icubam.db import gsheets
+from icubam.db import migrator
 from icubam.db import sqlite
 from icubam.db import store
-from icubam.db import migrator
+from icubam.db import synchronizer
 
 flags.DEFINE_string("config", "resources/config.toml", "Config file.")
 flags.DEFINE_string("dotenv_path", "resources/.env", "Config file.")
@@ -14,8 +16,15 @@ FLAGS = flags.FLAGS
 
 def main(unused_argv):
   cfg = config.Config(FLAGS.config, mode=FLAGS.mode, env_path=FLAGS.dotenv_path)
+  # First we migrate
   mgt = migrator.Migrator(cfg, FLAGS.old_db_path)
   mgt.run()
+
+  # Now we add the region that was not in the old one.
+  sheets = gsheets.SheetsDB(cfg.TOKEN_LOC, cfg.SHEET_ID)
+  sync = synchronizer.StoreSynchronizer(sheets, mgt.new_db)
+  sync.sync_icus()
+
 
 
 if __name__ == "__main__":
