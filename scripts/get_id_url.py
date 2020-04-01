bcc4f150f51c24600fceb9263ab6c086b036832a
ROUVREAU Vincent <vincent.rouvreau@inria.fr>
2020-04-01 08:21:42 +0200

    Use the store to access the db

--- scripts/get_id_url.py
+++ scripts/get_id_url.py
@@ -4,7 +4,7 @@ from absl import app
 from absl import flags
 from icubam import config
 from icubam.www import token
-from icubam.db import sqlite
+from icubam.db import store
 
 flags.DEFINE_string('config', 'resources/config.toml', 'Config file.')
 flags.DEFINE_string('dotenv_path', None, 'Optionally specifies the .env path.')
@@ -13,8 +13,7 @@ FLAGS = flags.FLAGS
 
 def main(argv):
   cfg = config.Config(FLAGS.config, mode=FLAGS.mode, env_path=FLAGS.dotenv_path)
-  sqldb = sqlite.SQLiteDB(cfg.db.sqlite_path)
-  icus = sqldb.get_icus()
+  sqldb = store.create_store_for_sqlite_db(cfg)
   encoder = token.TokenEncoder(cfg)
   for user in sqldb.get_users():
     for icu in user.icus:
