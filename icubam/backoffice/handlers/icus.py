3a2d863c1ee74656687ce716b64a26e1ff48a9b8
Olivier Teboul <oliviert@google.com>
2020-04-05 14:34:10 +0200

    many fixes in backoffice

--- icubam/backoffice/handlers/icus.py
+++ icubam/backoffice/handlers/icus.py
@@ -44,7 +44,7 @@ class ICUHandler(base.BaseHandler):
     icu = icu if icu is not None else store.ICU()
     if icu.is_active is None:
       icu.is_active = True
-    self.render("icu.html", icu=icu, regions=regions, error=error)
+    return self.render("icu.html", icu=icu, regions=regions, error=error)
 
   @tornado.web.authenticated
   def get(self):
