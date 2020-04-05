15a3dac48776fc30a202c5df09a641b5bf20fb58
Olivier Teboul <oliviert@google.com>
2020-04-06 00:27:51 +0200

    fixing tests

--- icubam/backoffice/server_test.py
+++ icubam/backoffice/server_test.py
@@ -29,7 +29,7 @@ class ServerTestCase(tornado.testing.AsyncHTTPTestCase):
     return super().fetch('/' + self.app.root + '/' + url, **kwargs)
 
   def test_homepage_without_cookie(self):
-    response = self.fetch(home.HomeHandler.ROUTE)
+    response = self.fetch('')
     self.assertEqual(response.code, 200)
 
   def test_login(self):
