c277cde2ea59c24496ead880e652140b218d1e6e
Olivier Teboul <oliviert@google.com>
2020-04-07 19:26:09 +0200

    adding links and validation

--- icubam/backoffice/handlers/tokens.py
+++ icubam/backoffice/handlers/tokens.py
@@ -1,10 +1,13 @@
 from absl import logging
+import os.path
 import tornado.escape
 import tornado.web
 from typing import Optional
 
 from icubam.backoffice.handlers import base
 from icubam.backoffice.handlers import home
+from icubam.www.handlers import home as www_home
+from icubam.www.handlers import db as www_db
 from icubam.db import store
 
 
@@ -12,10 +15,29 @@ class ListTokensHandler(base.AdminHandler):
 
   ROUTE = "list_tokens"
 
+  def prepare_for_table(self, client):
+    result = [{
+        'key': 'name',
+        'value': client.email,
+        'link': f'{TokenHandler.ROUTE}?id={client.external_client_id}'}
+    ]
+    client_dict = dict()
+    for key in ['access_key', 'is_active', 'expiration_date']:
+      client_dict[key] = getattr(client, key, None)
+    result.extend(self.format_list_item(client_dict))
+    for handler in [www_home.MapByAPIHandler, www_db.DBHandler]:
+      route = handler.ROUTE.strip('/').split('/')[0]
+      if handler == www_db.DBHandler:
+        route += '/bedcounts'
+      args = f'?API_KEY={client.access_key}'
+      url = os.path.join(self.config.server.base_url, route + args)
+      result.append({'key': route, 'value': 'link', 'link': url})
+    return result
+
   @tornado.web.authenticated
   def get(self):
     clients = self.db.get_external_clients()
-    data = [self.format_list_item(client.to_dict()) for client in clients]
+    data = [self.prepare_for_table(client) for client in clients]
     self.render_list(
       data=data, objtype='Acces Tokens', create_handler=TokenHandler)
 
@@ -34,12 +56,15 @@ class TokenHandler(base.AdminHandler):
 
   def do_render(self, user: Optional[store.User], error=False):
     user = user if user is not None else store.ExternalClient()
+    if user.is_active is None:
+      user.is_active = True
     return self.render("token.html", user=user, error=error,
                        list_route=ListTokensHandler.ROUTE)
 
   @tornado.web.authenticated
   def post(self):
     values = self.parse_from_body(store.ExternalClient)
+    values["is_active"] = values.get("is_active", "off") == 'on'
     id_key = 'external_client_id'
     token_id = values.pop(id_key, '')
     try:
