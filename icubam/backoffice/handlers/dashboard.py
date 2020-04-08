5bcfa96e7ec685bc7d96f97b900aa7278108f2fc
Olivier Teboul <oliviert@google.com>
2020-04-08 15:26:06 +0200

    reverting dashboard (other pr)

--- icubam/backoffice/handlers/dashboard.py
+++ icubam/backoffice/handlers/dashboard.py
@@ -14,38 +14,23 @@ class ListBedCountsHandler(base.BaseHandler):
     super().initialize()
     self.link_fn = updater.Updater(self.config, self.db).get_url
 
-
-  def make_link(self, icu):
-    return {
+  def prepare_data(self, bed_count) -> list:
+    icu =  bed_count.icu
+    result = [{
       'key': 'icu (update link)',
       'value': icu.name,
       'link': self.link_fn(icu.icu_id, icu.name)
-    }
-
-  def prepare_data(self, bed_count) -> list:
-    result = []
-    if bed_count.icu is not None:
-      result = [self.make_link(bed_count.icu)]
+    }]
 
     bed_count_dict = bed_count.to_dict(max_depth=0)
     locale = self.get_user_locale()
-    last = bed_count_dict.pop('last_modified', None)
-    last = None if last is None else last.timestamp()
-    for key in ['rowid', 'icu_id', 'message', 'create_date', 'icu']:
+    last = bed_count_dict.pop('last_modified').timestamp()
+    for key in ['rowid', 'icu_id', 'message', 'create_date']:
       bed_count_dict.pop(key, None)
     bed_count_dict['since_update'] = time_utils.localewise_time_ago(
       last, locale=locale)
     result.extend(self.format_list_item(bed_count_dict))
-    return result
 
-  def get_empty_icus(self):
-    icus = self.db.get_managed_icus(self.user.user_id)
-    empty_icus = [i for i in icus if not i.bed_counts]
-    result = []
-    for icu in empty_icus:
-      curr = self.prepare_data(store.BedCount())
-      curr.insert(0, self.make_link(icu))
-      result.append(curr)
     return result
 
   @tornado.web.authenticated
@@ -56,6 +41,5 @@ class ListBedCountsHandler(base.BaseHandler):
       bed_counts = self.db.get_visible_bed_counts_for_user(self.user.user_id)
 
     data = [self.prepare_data(bd) for bd in bed_counts]
-    data.extend(self.get_empty_icus())
     return self.render_list(
       data=data, objtype='Bed Counts', create_handler=None)
