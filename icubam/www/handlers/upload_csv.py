259b3f8caef4e7ad9c91f39ac68becdb62e3bc02
Gabriel Dulac-Arnold <gabriel@technonautes.fr>
2020-04-08 20:44:14 +0200

    Sanitized upload name some.

--- icubam/www/handlers/upload_csv.py
+++ icubam/www/handlers/upload_csv.py
@@ -20,7 +20,17 @@ class UploadHandler(base.BaseHandler):
 
   def gen_path(self, f_name):
     time_str = time.strftime("%Y-%m-%d-%H:%M:%S")
-    return os.path.join(self.upload_path, f"{time_str}-{f_name}")
+    # Sanitize the name some:
+    f_name = os.path.split(f_name)[1]
+    f_name, f_ext = os.path.splitext(f_name)
+    # Clean out non-alphanumerics (need to split ext first or we lose the '.'):
+    f_name, f_ext = [
+      "".join(c
+              for c in f_name
+              if c.isalnum())[:64]
+      for f_name in [f_name, f_ext]
+    ]
+    return os.path.join(self.upload_path, f"{time_str}-{f_name}.{f_ext}")
 
   def initialize(self, upload_path, config, db_factory):
     self.upload_path = upload_path
@@ -30,6 +40,7 @@ class UploadHandler(base.BaseHandler):
   def post(self):
     file = self.request.files["file"][0]
     file_path = self.gen_path(file["filename"])
+
     try:
       with open(file_path, "wb") as f:
         f.write(file["body"])
