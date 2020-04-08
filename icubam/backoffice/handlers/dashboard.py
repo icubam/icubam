94b1588ae52aa02e764e0007116b275d264f370c
Olivier Teboul <oliviert@google.com>
2020-04-08 11:45:51 +0200

    improving dashboard creation

--- icubam/backoffice/handlers/dashboard.py
+++ icubam/backoffice/handlers/dashboard.py
@@ -14,19 +14,14 @@ class ListBedCountsHandler(base.BaseHandler):
     super().initialize()
     self.link_fn = updater.Updater(self.config, self.db).get_url
 
-
-  def make_link(self, icu):
-    return {
+  def prepare_data(self, icu) -> list:
+    result = [{
       'key': 'icu (update link)',
       'value': icu.name,
       'link': self.link_fn(icu.icu_id, icu.name)
-    }
+    }]
 
-  def prepare_data(self, bed_count) -> list:
-    result = []
-    if bed_count.icu is not None:
-      result = [self.make_link(bed_count.icu)]
-
+    bed_count = icu.bed_counts[-1] if icu.bed_counts else store.BedCount()
     bed_count_dict = bed_count.to_dict(max_depth=0)
     locale = self.get_user_locale()
     last = bed_count_dict.pop('last_modified', None)
@@ -38,24 +33,9 @@ class ListBedCountsHandler(base.BaseHandler):
     result.extend(self.format_list_item(bed_count_dict))
     return result
 
-  def get_empty_icus(self):
-    icus = self.db.get_managed_icus(self.user.user_id)
-    empty_icus = [i for i in icus if not i.bed_counts]
-    result = []
-    for icu in empty_icus:
-      curr = self.prepare_data(store.BedCount())
-      curr.insert(0, self.make_link(icu))
-      result.append(curr)
-    return result
-
   @tornado.web.authenticated
   def get(self):
-    if self.user.is_admin:
-      bed_counts = self.db.get_latest_bed_counts()
-    else:
-      bed_counts = self.db.get_visible_bed_counts_for_user(self.user.user_id)
-
-    data = [self.prepare_data(bd) for bd in bed_counts]
-    data.extend(self.get_empty_icus())
+    icus = self.db.get_managed_icus(self.user.user_id)
+    data = [self.prepare_data(icu) for icu in icus if icu.is_active]
     return self.render_list(
       data=data, objtype='Bed Counts', create_handler=None)
