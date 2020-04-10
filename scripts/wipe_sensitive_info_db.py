39027c159f97f6a49a903cce5729c63fbe7bdcd4
Romain PRIMET <romain.primet@inria.fr>
2020-04-10 11:09:01 +0200

    make filename a flag

--- scripts/wipe_sensitive_info_db.py
+++ scripts/wipe_sensitive_info_db.py
@@ -12,11 +12,15 @@ import sys
 from icubam.db.wipe import wipe_db
 
 flags.DEFINE_boolean(
-  "force", False, "Do not prompt user confirmation and do not print query."
+  "force", False, "Do not prompt user confirmation."
 )
 flags.DEFINE_boolean(
   "keep_beds", False, "Whether to keep bed occupation data."
 )
+flags.DEFINE_string(
+  "filename", None, 'DB file name.'
+)
+flags.mark_flag_as_required('filename')
 FLAGS = flags.FLAGS
 
 def wipe_db_path(path, keep_beds):
@@ -25,17 +29,12 @@ def wipe_db_path(path, keep_beds):
 
 
 def main(argv):
-  if len(argv) < 2:
-    print(f"Usage: {argv[0]} DB_PATH")
-    sys.exit(-1)
-
   if not FLAGS.force:
     if not click.confirm(
       "WARNING: THIS WILL WIPE THE DATABASE IN-PLACE. CONTINUE?", err=True
     ):
-      sys.exit(0)
-
-  wipe_db_path(argv[1], FLAGS.keep_beds)
+      return
+  wipe_db_path(FLAGS.filename, FLAGS.keep_beds)
 
 
 if __name__ == "__main__":
