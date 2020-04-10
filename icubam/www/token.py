46e487823f1d004d5c86f3deb8ac0ff13402868c
Olivier Teboul <oliviert@google.com>
2020-04-10 21:48:54 +0200

    fixing yapf

--- icubam/www/token.py
+++ icubam/www/token.py
@@ -9,9 +9,8 @@ class TokenEncoder:
     self.config = config
 
   def encode(self, value) -> str:
-    return jwt.encode({self.KEY: value},
-                      self.config.JWT_SECRET,
-                      algorithm='HS256').decode()
+    data = {self.KEY: value}
+    return jwt.encode(data, self.config.JWT_SECRET, algorithm='HS256').decode()
 
   def decode(self, token: str):
     try:
