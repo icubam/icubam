38f6c5e4706d7a5a82735e27886d988731885ade
praverdy <pierre-guillaume.raverdy@inria.fr>
2020-04-13 01:25:59 +0200

    Trim generated docker image and removes conda references

--- docker/start_server_sms.sh
+++ docker/start_server_sms.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# conda init, activate environment
-# and add current module (icubam) in edit/develop mode
-conda init bash
-conda activate icubam
-
 # launch the server
 echo "launch sms server in mode $ENV_MODE"
 python3 scripts/schedule_sms.py --mode=$ENV_MODE --config=/home/icubam/resources/config.toml
