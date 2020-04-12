26af20f1f742f42f692fa27c222fc71de2536a7c
olivierteboul <54403230+olivierteboul@users.noreply.github.com>
2020-04-13 00:19:22 +0200

    fixing bug and removing stuff (#246)
    * fixing bug and removing stuff
    
    * fixing bug and removing stuff
    
    Co-authored-by: dulacarnold <gabriel@technonautes.fr>

--- icubam/backoffice/handlers/icus.py
+++ icubam/backoffice/handlers/icus.py
@@ -34,8 +34,8 @@ class ListICUsHandler(base.BaseHandler):
       icus = self.db.get_managed_icus(self.user.user_id)
 
     data = [self.prepare_for_table(icu) for icu in icus]
-    return self.render(
-      "list.html", data=data, objtype='ICUs', create_route=ICUHandler.ROUTE
+    return self.render_list(
+      data=data, objtype='ICUs', create_handler=ICUHandler
     )
 
 
