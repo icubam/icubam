293dbadeca91f02c39d887f956de2c70cab42124
Olivier Teboul <oliviert@google.com>
2020-04-14 18:11:18 +0200

    fixing test

--- icubam/www/handlers/home.py
+++ icubam/www/handlers/home.py
@@ -4,7 +4,6 @@ import tornado.web
 import icubam
 from icubam.db import store
 from icubam.www.handlers import base
-from icubam.www import token
 from icubam import map_builder
 
 
@@ -14,7 +13,7 @@ class HomeHandler(base.BaseHandler):
 
   def initialize(self, config, db_factory):
     super().initialize(config, db_factory)
-    self.token_encoder = token.TokenEncoder(self.config)
+    self.icu = None
 
   @tornado.web.authenticated
   def get(self):
