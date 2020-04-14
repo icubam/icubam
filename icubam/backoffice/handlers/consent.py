5bc98112215e4481110eaf95a9948d143e67fc0c
Olivier Teboul <oliviert@google.com>
2020-04-14 17:12:49 +0200

    consent reset

---
+++ icubam/backoffice/handlers/consent.py
@@ -0,0 +1,29 @@
+"""Creating/edition of ICUs."""
+from absl import logging
+import json
+import tornado.web
+
+from icubam.backoffice.handlers import base
+
+
+class ConsentResetHandler(base.BaseHandler):
+  ROUTE = "reset_consent"
+
+  def answer(self, msg, error=None):
+    logging.error(f"{msg}: {error}")
+    return self.write(json.dumps({'msg': msg, 'error': error}))
+
+  @tornado.web.authenticated
+  def post(self):
+    try:
+      user_id = json.loads(self.request.body.decode())
+    except Exception as err:
+      return self.answer(f'Cannot read request', error=err)
+
+    try:
+      self.db.update_user(
+        self.current_user.user_id, user_id, dict(consent=None)
+      )
+    except Exception as err:
+      return self.answer(f'Cannot reset consent: {e}', error=err)
+    return self.answer('OK!')
