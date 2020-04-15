0b116a0b1441afd808a3331f97b81fe9038b0408
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2020-04-15 16:29:29 -0400

    Add basic ErrorHandler and template in www

---
+++ icubam/www/handlers/error.py
@@ -0,0 +1,12 @@
+import icubam
+from icubam.www.handlers import base
+
+
+class ErrorHandler(base.BaseHandler):
+  ROUTE = '/error'
+
+  def initialize(self, config, db_factory):
+    super().initialize(config, db_factory)
+
+  def get(self):
+    return self.render('error.html')
