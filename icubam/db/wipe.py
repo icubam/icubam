0adf25b4590a1b33017e6a5ef35fffe9d477255a
Romain Primet <romain.primet@inria.fr>
2020-04-20 13:31:55 +0200

    Wipe using SQLAlchemy API + reset admin option (#344)
    * Wipe using SQLAlchemy API + reset admin option

--- icubam/db/wipe.py
+++ icubam/db/wipe.py
@@ -1,49 +1,71 @@
+from icubam.db.store import BedCount, ExternalClient, ICU, User
+
 ALTERATIONS = {
-  "bed_counts": [
-    ("n_ncovid_free", 3),
-    ("n_ncovid_occ", 5),
-    ("n_covid_deaths", 0),
-    ("n_covid_healed", 0),
-    ("n_covid_refused", 0),
-    ("n_covid_transfered", 0),
-    ("message", '"Have a nice day!"'),
-  ],
-  "icus": [("telephone", "33120000000 + icus.icu_id")
-           ],  # we reuse the ID to respect telephone uniqueness constraint
-  "users": [
-    ("name", '"Jean Dumont"'),
-    ("email", '"jean.dumont@example.org"'),
-    ("telephone", "33600000001 + users.user_id"),
-    ("password_hash", 42),
-  ],
-  "external_clients": [
-    ("name", '"Jeanne Externe"'),
-    ("email", '"jeanne.externe@example.org"'),
-    ("telephone", "33600000001 + external_clients.external_client_id"),
-  ],
+  BedCount: {
+    "n_ncovid_free": 3,
+    "n_ncovid_occ": 5,
+    "n_covid_deaths": 0,
+    "n_covid_healed": 0,
+    "n_covid_refused": 0,
+    "n_covid_transfered": 0,
+    "message": "Have a nice day!",
+  },
+  ICU: {
+    "telephone": "33120000000"
+  },
+  User: {
+    "name": "Jean Dumont",
+    "email": "jean.dumont@example.org",
+    "telephone": "33600000001",
+    "password_hash": 42,
+  },
+  ExternalClient: {
+    "name": "Jeanne Externe",
+    "email": "jeanne.externe@example.org",
+    "telephone": "33600000001",
+  },
 }
 
 
-def wipe_db(conn, keep_beds):
+def wipe_db(
+  store, keep_beds, reset_admin=False, admin_email=None, admin_pass=None
+):
   """
   Wipe sensitive information from a database.
 
   Parameters
   ----------
-  conn: sqlite3 connection
+  store: icubam store
 
   keep_beds: bool
       True if bed information should be kept
+
+  reset_admin: bool
+      True if admins should be removed and a 'test' admin created.
+      If True, require admin_email and admin_pass to be set.
+
+  admin_email: str
+      Email of the newly-created admin user (if resetting admins)
+
+  admin_pass: str
+      Password of the newly-created admin user (if resetting admins)
 
   Returns
   -------
   None
   """
+
   if not keep_beds:
-    ALTERATIONS["bed_counts"].extend([("n_covid_free", 2), ("n_covid_occ", 4)])
+    ALTERATIONS[BedCount].update({"n_covid_free": 2, "n_covid_occ": 4})
   for alteration in ALTERATIONS.items():
-    affectations = ",".join([f"{k} = {v}" for (k, v) in alteration[1]])
-    query = f"UPDATE {alteration[0]} SET {affectations};"
-    conn.execute(query)
-  conn.commit()
-  conn.close()
+    store._session.query(alteration[0]).update(values=alteration[1])
+
+  if reset_admin:
+    if None in [admin_email, admin_pass]:
+      raise ValueError("admin_email and admin_pass must be defined")
+    store._session.query(User).filter(User.is_admin == True
+                                      ).update({'is_admin': False})
+    hash = store.get_password_hash(admin_pass)
+    store.add_user(
+      User(name="admin", email=admin_email, password_hash=hash, is_admin=True)
+    )
