979911d05841004aeb944ee5eae81c8ac17b5a80
Roman Yurchak <rth.yurchak@gmail.com>
2020-04-21 15:04:40 +0200

    Documentation

--- icubam/predicu/__main__.py
+++ icubam/predicu/__main__.py
@@ -1,16 +1,16 @@
-from pathlib import Path
+import asyncio
 import datetime
 import logging
 import os
 import sys
+from pathlib import Path
 
-import asyncio
 import click
 
 from icubam.config import Config
 from icubam.db.store import create_store_factory_for_sqlite_db
-from icubam.predicu.data import load_bedcounts
 from icubam.predicu.analytics_server import AnalyticsCallback
+from icubam.predicu.data import load_bedcounts
 
 
 @click.group()
@@ -52,17 +52,28 @@ def export_data(
   logging.info("export DONE.")
 
 
-@cli.command(name="make_dashboard_plots")
+@cli.command(name="make_dashboard_plots", help="Make dasboard plots ")
 @click.option(
   "--config", "-o", type=str, required=True, help="ICUBAM config file."
 )
-@click.option("--output-dir", "-o", default="/tmp", type=str)
+@click.option(
+  "--output-dir",
+  "-o",
+  type=str,
+  help=(
+    "Output directory where to store plots. If not provided "
+    "config.backoffice.extra_plots_dir is used."
+  )
+)
 def make_dasboard_plots(config, output_dir):
   config = Config(config)
   # Make sure DB path exists, to avoid creating an empty one
   if not Path(config.db.sqlite_path).exists():
     click.echo(f"Could not find DB at {config.db.sqlite_path}!", err=True)
     sys.exit(1)
+
+  if output_dir is None:
+    output_dir = config.backoffice.extra_plots_dir
 
   output_dir = Path(output_dir)
   if not output_dir.exists():
