88c5ffd52b81e89d9a0291fe920c2e8238922d4c
Olivier Teboul <oliviert@google.com>
2020-04-21 13:42:55 +0200

    map ok

--- icubam/www/static/map.js
+++ icubam/www/static/map.js
@@ -70,7 +70,6 @@ function addMarker(obj, map) {
 
   var infowindow = new google.maps.InfoWindow({
     content: obj.popup,
-    maxWidth: 600,
   });
 
   let icon_url = "http://maps.google.com/mapfiles/ms/icons/"
