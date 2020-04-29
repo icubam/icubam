3065d6bf8e72eeedce70d2353c1e6bfbfc0e4ae8
Roman Yurchak <rth.yurchak@gmail.com>
2020-04-29 16:44:04 +0200

    Add test_enforce_daily_values_for_all_icus

--- icubam/analytics/tests/test_preprocessing.py
+++ icubam/analytics/tests/test_preprocessing.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pytest
+import pandas as pd
 import sqlalchemy as sqla
+from numpy.testing import assert_allclose
 
 from icubam.analytics import dataset, preprocessing
 from icubam.db import store
@@ -26,3 +28,47 @@ def test_bedcounts_data_preprocessing(fake_db):
     for col in dataset.CUM_COLUMNS:
       diffs = dg[col].diff(1).fillna(0).values
       assert np.all(diffs >= 0)
+
+
+def test_enforce_daily_values_for_all_icus():
+  df_raw = pd.DataFrame({
+    "icu_name": ['A', 'B', 'A', 'A', 'A'],
+    "department": ['D1', 'D2', 'D1', 'D1', 'D1'],
+    "region": ['R1', 'R3', 'R1', 'R1', 'R1'],
+    "n_covid_deaths": [0, 1, 2, 3, 5],
+    "region_id": [1, 2, 1, 1, 1],
+    "datetime":
+    pd.to_datetime([
+      '2020-01-01', '2020-01-01', '2020-01-02', '2020-01-03', '2020-01-04'
+    ]),
+    "create_date":
+    pd.to_datetime([
+      '2020-01-01', '2020-01-01', '2020-01-02', '2020-01-03', '2020-01-04'
+    ]),
+  })
+  df_raw['date'] = df_raw["datetime"].dt.date
+  for key in dataset.CUM_COLUMNS:
+    df_raw[key] = [0, 1, 2, 3, 5]
+
+  for key in dataset.NCUM_COLUMNS:
+    df_raw[key] = [0, 3, 4, 2, 5]
+  assert df_raw.shape == (5, 15)
+
+  df = preprocessing.enforce_daily_values_for_all_icus(df_raw)
+  assert df.shape == (8, 15)
+  # For ICU A the data is unchanged
+  assert_allclose(
+    df.loc[df['icu_name'] == 'A', dataset.CUM_COLUMNS[0]].values, [0, 2, 3, 5]
+  )
+  assert_allclose(
+    df.loc[df['icu_name'] == 'A', dataset.NCUM_COLUMNS[0]].values,
+    [0, 4, 2, 5]
+  )
+  # For ICU B we fill missing days
+  assert_allclose(
+    df.loc[df['icu_name'] == 'B', dataset.CUM_COLUMNS[0]].values, [1, 1, 1, 1]
+  )
+  assert_allclose(
+    df.loc[df['icu_name'] == 'B', dataset.NCUM_COLUMNS[0]].values,
+    [3, 3, 3, 3]
+  )
