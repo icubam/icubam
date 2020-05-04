fe5d0c2475438eddcd4b21ec0f00869accc9a1a3
Roman Yurchak <rth.yurchak@gmail.com>
2020-05-04 17:24:15 +0200

    Add --create-admin option to scripts/update_password.py

--- scripts/update_password.py
+++ scripts/update_password.py
@@ -13,6 +13,7 @@ flags.DEFINE_string(
 )
 flags.DEFINE_string('email', None, 'File for the db.')
 flags.DEFINE_string('password', None, 'File for the db.')
+flags.DEFINE_bool('create-admin', False, 'Create a new user')
 FLAGS = flags.FLAGS
 
 
@@ -20,19 +21,29 @@ def main(argv):
   cfg = config.Config(FLAGS.config, env_path=FLAGS.dotenv_path)
   factory = store.create_store_factory_for_sqlite_db(cfg)
   db = factory.create()
-  user_id = db.get_user_by_email(FLAGS.email)
-  if user_id is None:
-    logging.error(f"No user for email {FLAGS.email}")
-    return
+  password_hash = db.get_password_hash(FLAGS.password)
 
-  admins = db.get_admins()
-  if not admins:
-    admin_id = db.add_default_admin()
+  if FLAGS['create-admin']:
+    user = store.User(
+      name=FLAGS.email,
+      email=FLAGS.email,
+      password_hash=password_hash,
+      is_admin=True
+    )
+    db.add_user(user)
   else:
-    admin_id = admins[0].user_id
+    user_id = db.get_user_by_email(FLAGS.email)
+    if user_id is None:
+      logging.error(f"No user for email {FLAGS.email}")
+      return
 
-  hash = db.get_password_hash(FLAGS.password)
-  db.update_user(admin_id, user_id, dict(password_hash=hash))
+    admins = db.get_admins()
+    if not admins:
+      admin_id = db.add_default_admin()
+    else:
+      admin_id = admins[0].user_id
+
+    db.update_user(admin_id, user_id, dict(password_hash=password_hash))
 
 
 if __name__ == '__main__':
