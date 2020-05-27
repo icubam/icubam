3c0eea68684884a5647a852bf7c027a5844586da
olivierteboul <54403230+olivierteboul@users.noreply.github.com>
2020-05-27 12:09:50 +0200

    WIP: analytics client and server (#407)
    * analytics server
    
    * analytics client and server
    
    * fixing tests
    
    * testing passing
    
    * Move extra_plots_dir config entry from backoffice to analytics
    
    * Update analytics service documentation
    
    * Pin flake8 to latest working version
    
    * Fix running scripts/gen_plots.py for all plots
    
    * Fix bug in plot_each_region
    
    * Revert "Pin flake8 to latest working version"
    
    This reverts commit 996f8fae09422eee42ec8dda33b134895a3f7a3c.
    
    * commit
    
    * caching stuff
    
    * fixing tests
    
    Co-authored-by: Roman Yurchak <rth.yurchak@gmail.com>
    Co-authored-by: Gabriel Dulac-Arnold <gabriel@technonautes.fr>

---
+++ icubam/analytics/server.py
@@ -0,0 +1,39 @@
+import numbers
+import tornado.ioloop
+
+from icubam import base_server, sentry
+from icubam.analytics import generator, dataset
+from icubam.analytics.handlers import dataset as dataset_handler
+
+
+class AnalyticsServer(base_server.BaseServer):
+  """Runs analytics jobs."""
+  def __init__(self, config, port):
+    super().__init__(config, port)
+    self.port = port if port is not None else self.config.analytics.port
+    self.db = self.db_factory.create()
+    sentry.maybe_init_sentry(config, server_name='analytics')
+
+    # Periodic data generation and cache have the same frequency.
+    frequency = config.analytics.generate_plots_every
+    if not isinstance(frequency, numbers.Number) or frequency <= 0:
+      frequency = None
+    self.dataset = dataset.Dataset(self.db, ttl=frequency - 1)
+    self.generator = generator.PlotGenerator(
+      self.config, self.db, self.dataset, frequency
+    )
+    self.generator.register(tornado.ioloop)
+
+  def make_app(self):
+    self.add_handler(
+      dataset_handler.DatasetHandler,
+      config=self.config,
+      db_factory=self.db_factory,
+      dataset=self.dataset,
+      upload_path=self.config.server.upload_dir,
+    )
+    # Only accepts request from same host
+    app_routes = [
+      (tornado.routing.HostMatches(r'(localhost|127\.0\.0\.1)'), self.routes)
+    ]
+    return tornado.web.Application(app_routes)
