5fe2694cecec7f969de4563312ad9b1d4998be50
epoiseau <eric.poiseau@inria.fr>
2020-11-10 10:28:44 +0100

    Update of subtitle full display according to feedback to Antoine

--- icubam/www/static/map.js
+++ icubam/www/static/map.js
@@ -241,28 +241,28 @@ function plotMap(data, center, covid_status=true) {
    titleClone.getElementsByClassName('subtitle-covid-pos')[0].style.display = 'block';
    titleClone.getElementsByClassName('subtitle-covid-neg')[0].style.display = 'none';
    titleClone.getElementsByClassName('subtitle-covid-all')[0].style.display = 'none';
-   titleClone.getElementsByClassName('subtitle-full-pos')[0].style.display = 'block';
-   titleClone.getElementsByClassName('subtitle-full-neg')[0].style.display = 'none';
-   titleClone.getElementsByClassName('subtitle-full-all')[0].style.display = 'none';
-   titleClone.getElementsByClassName('subtitle-cluster')[0].style.display = 'none';
+//   titleClone.getElementsByClassName('subtitle-full-pos')[0].style.display = 'block';
+//   titleClone.getElementsByClassName('subtitle-full-neg')[0].style.display = 'none';
+//   titleClone.getElementsByClassName('subtitle-full-all')[0].style.display = 'none';
+//   titleClone.getElementsByClassName('subtitle-cluster')[0].style.display = 'none';
   }
   else if (covid_status == false){
     titleClone.getElementsByClassName('subtitle-covid-neg')[0].style.display = 'block';
     titleClone.getElementsByClassName('subtitle-covid-pos')[0].style.display = 'none';
     titleClone.getElementsByClassName('subtitle-covid-all')[0].style.display = 'none';
-    titleClone.getElementsByClassName('subtitle-full-pos')[0].style.display = 'none';
-    titleClone.getElementsByClassName('subtitle-full-neg')[0].style.display = 'block';
-    titleClone.getElementsByClassName('subtitle-full-all')[0].style.display = 'none';
-    titleClone.getElementsByClassName('subtitle-cluster')[0].style.display = 'none';
+//    titleClone.getElementsByClassName('subtitle-full-pos')[0].style.display = 'none';
+//    titleClone.getElementsByClassName('subtitle-full-neg')[0].style.display = 'block';
+//    titleClone.getElementsByClassName('subtitle-full-all')[0].style.display = 'none';
+//    titleClone.getElementsByClassName('subtitle-cluster')[0].style.display = 'none';
   }
   else {
     titleClone.getElementsByClassName('subtitle-covid-all')[0].style.display = 'block';
     titleClone.getElementsByClassName('subtitle-covid-neg')[0].style.display = 'none';
     titleClone.getElementsByClassName('subtitle-covid-pos')[0].style.display = 'none';
-    titleClone.getElementsByClassName('subtitle-full-pos')[0].style.display = 'none';
-    titleClone.getElementsByClassName('subtitle-full-neg')[0].style.display = 'none';
-    titleClone.getElementsByClassName('subtitle-full-all')[0].style.display = 'block';
-    titleClone.getElementsByClassName('subtitle-cluster')[0].style.display = 'none';
+//    titleClone.getElementsByClassName('subtitle-full-pos')[0].style.display = 'none';
+//    titleClone.getElementsByClassName('subtitle-full-neg')[0].style.display = 'none';
+//    titleClone.getElementsByClassName('subtitle-full-all')[0].style.display = 'block';
+//    titleClone.getElementsByClassName('subtitle-cluster')[0].style.display = 'none';
   }
 //  var dispEl = covid_status ? 'pos' : 'neg'
 //  var hideEl = !covid_status ? 'pos' : 'neg'
